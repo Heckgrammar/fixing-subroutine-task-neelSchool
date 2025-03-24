f70de0b6e3089b87aa755286b62480d448cc5dc8
Neel Mehendale <144788333+neelSchool@users.noreply.github.com>
2025-03-24 08:32:11 +0000

    done

--- FixSubroutineTask/Program.cs
+++ FixSubroutineTask/Program.cs
@@ -4,15 +4,14 @@
     {
         static void Main(string[] args)
         {
-            for(int i = 8;i >= 0; i++) {
+            for(int i = 0;i < 8; i++) {
                 Console.WriteLine(diffCurrencies(i));            
             }
         }
         static string diffCurrencies(int x)
         {
             string[] currencies = { "baht", "dollar", "euro", "koruna", "lira", "rand", "rupee", "yen" };
-            return currencies[1];
-
+            return currencies[x];
         }
     }
 }
